00c8a534dcbf63080e38b842f4dae820d5809554
Pranto Kumar Saha <69887925+prantokumar@users.noreply.github.com>
2022-04-22 10:23:47 +0600

    Development pks (#2)
    * dev branch commit by pks(22/04/2022)
    
    * dev branch commit-2 by pks(22/04/2022)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 console.log('javascript initialized');
 
 //variables and values in javascript
+var myName = 'Pranto Kumar Saha'; //best practice : naming convention always be written as camelCase
+console.log(myName);
+var myname = document.getElementById("myname");
+myname.innerHTML = myName;
+
+//datatypes in javascript
+//there are mainly six/6 type of datatypes (undefined, boolean, number, string, bigint, symbol)
 var myName = 'Pranto Kumar Saha';
 console.log(myName);
