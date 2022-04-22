edc4c64efed04ed072ff8b173748e6ba26027d76
prantokumar <officialprantosaha@gmail.com>
2022-04-22 10:18:27 +0600

    dev branch commit-2 by pks(22/04/2022)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 console.log('javascript initialized');
 
 //variables and values in javascript
-var myName = 'Pranto Kumar Saha';
+var myName = 'Pranto Kumar Saha'; //best practice : naming convention always be written as camelCase
 console.log(myName);
+var myname = document.getElementById("myname");
+myname.innerHTML = myName;
+
+//datatypes in javascript
+//there are mainly six/6 type of datatypes (undefined, boolean, number, string, bigint, symbol)
+
